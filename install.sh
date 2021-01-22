d6933bfbe4a91ea3f96a3a916fa8a04191aa09f5
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2021-01-22 17:02:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # credits goes to f8al (https://github.com/f8al)
 
+sudo su
 cd $HOME
 sudo add-apt-repository -y ppa:longsleep/golang-backports &> /dev/null
 sudo apt update &> /dev/null
@@ -9,15 +10,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 mkdir -p $HOME/go/{bin,src,pkg}
 chmod +x $HOME/go/bin
 echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
-echo "export GOROOT=\"/usr/local/go/libexec\"" >> $HOME/.bashrc
-echo "export PATH=\"$PATH:${GOPATH}/bin:${GOROOT}/bin\"" >> $HOME/.bashrc
+source $HOME/.bashrc
 mkdir -p $HOME/tools/
 git clone https://github.com/aboul3la/Sublist3r.git $HOME/tools/Sublist3r &> /dev/null
 pip3 install -r $HOME/tools/Sublist3r/requirements.txt &> /dev/null
 wget https://raw.githubusercontent.com/haccer/subjack/master/fingerprints.json $HOME/tools/ &> /dev/null 
-git clone https://github.com/Edu4rdSHL/findomain.git $HOME/tools/findomain &> /dev/null
-cd $HOME/tools/findomain && cargo build --release && cp $HOME/tools/findomain/target/release/findomain /usr/local/bin/
-rm -r $HOME/tools/findomain
+wget https://github.com/OWASP/Amass/releases/download/v3.11.1/amass_linux_amd64.zip
+unzip amass_linux_amd64.zip && mv amass /usr/bin/
 GO111MODULE=on go get -u -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder &> /dev/null
 go get -u github.com/tomnomnom/waybackurls &> /dev/null
 go get -u github.com/tomnomnom/gf &> /dev/null
@@ -40,3 +39,4 @@ git clone https://github.com/m4ll0k/takeover $HOME/tools/takeover
 cd $HOME/tools/takeover && python3 setup.py install
 echo "alias takeover.py='python3 $HOME/tools/takeover/takeover.py'" >> ~/.bashrc
 source ~/.bashrc
+pip3 install slackclient slacker
