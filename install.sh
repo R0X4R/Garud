5b6e8efefd7ec3bfa974e037461037ee7b7516a8
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2021-05-12 18:01:54 +0530

    fixed issues

--- install.sh
+++ install.sh
@@ -11,50 +11,38 @@ sudo apt-get install python3-pip
 sudo apt-get install ruby
 sudo apt-get install golang-go
 sudo apt install snapd
+sudo apt install cmake
 sudo snap install chromium
 sleep 2s
 clear
 echo "Installing essential tools"
 mkdir ~/tools
 git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r && cd ~/tools/Sublist3r && sudo pip3 install -r requirements.txt
-cd ~/tools/ && wget https://raw.githubusercontent.com/haccer/subjack/master/fingerprints.json
-git clone https://github.com/devanshbatham/ParamSpider ~/tools/ParamSpider && cd ~/tools/ParamSpider && sudo pip3 install -r requirements.txt
 git clone https://github.com/maurosoria/dirsearch.git ~/tools/dirsearch && cd ~/tools/dirsearch && sudo pip3 install -r requirements.txt
 git clone https://github.com/devanshbatham/OpenRedireX.git ~/tools/OpenRedireX && cd ~/tools/OpenRedireX && sudo pip3 install -r requirements.txt
+git clone https://github.com/defparam/smuggler.git ~/tools/smuggler
+git clone https://github.com/ameenmaali/urldedupe.git ~/tools/urldedupe && cd ~/tools/urldedupe && cmake CMakeLists.txt && make && mv urldedupe /usr/bin/
 cd
 mkdir ~/wordlists/
 cd ~/wordlists/ && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/big.txt
 wget https://gist.githubusercontent.com/Lopseg/33106eb13372a72a31154e0bbab2d2b3/raw/a79331799a70d0ae0ea906f2b143996d85f71de5/dicc.txt
 echo "Installing go-lang tools"
-go get -u github.com/tomnomnom/waybackurls
-GO111MODULE=on go get -u -v github.com/lc/gau
-go get github.com/hakluke/hakrawler
 go get github.com/michenriksen/aquatone
 go get -u github.com/tomnomnom/assetfinder
+GO111MODULE=on go get -u -v github.com/bp0lr/gauplus
 go get github.com/Emoe/kxss
-go get -u github.com/tomnomnom/hacks/get-title
 go get -u github.com/tomnomnom/qsreplace
-go get -u github.com/ffuf/ffuf
-GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/notify
+git clone https://github.com/projectdiscovery/dnsx.git; cd dnsx/cmd/dnsx; go build; mv dnsx /usr/bin/ &> /dev/null
+cd ~/tools/ && git clone https://github.com/projectdiscovery/notify.git; cd notify/cmd/notify; go build; mv notify /usr/bin/
 cd ~/tools/ && git clone https://github.com/hahwul/dalfox && cd dalfox && go install
 cd ~/tools/ && git clone https://github.com/dwisiswant0/crlfuzz && cd crlfuzz/cmd/crlfuzz && go build . && sudo mv crlfuzz /usr/bin/
+cd ~/tools/ && git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei && go build && mv nuclei /usr/bin/
+cd ~/tools/ && git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build && mv subfinder /usr/bin/
+cd ~/tools/ && wget https://github.com/projectdiscovery/httpx/releases/download/v1.0.6/httpx_1.0.6_linux_386.tar.gz && tar -xvf httpx_1.0.3_linux_386.tar.gz && mv httpx /usr/bin/
+cd ~/tools/ && wget https://github.com/OWASP/Amass/releases/download/v3.12.3/amass_linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/
 cd
 go get -u github.com/tomnomnom/anew
 go get -u github.com/tomnomnom/gf
-cd
-mkdir ~/tools-zip/
-cd ~/tools-zip/
-wget https://github.com/projectdiscovery/nuclei/releases/download/v2.2.0/nuclei_2.2.0_linux_386.tar.gz
-wget https://github.com/projectdiscovery/httpx/releases/download/v1.0.3/httpx_1.0.3_linux_386.tar.gz
-wget https://github.com/projectdiscovery/subfinder/releases/download/v2.4.6/subfinder_2.4.6_linux_386.tar.gz
-tar -xvf subfinder_2.4.6_linux_386.tar.gz
-tar -xvf httpx_1.0.3_linux_386.tar.gz
-tar -xvf nuclei_2.2.0_linux_386.tar.gz
-mv nuclei /usr/bin/ 
-mv subfinder /usr/bin/ 
-mv httpx /usr/bin/ 
-cd
-rm -rf ~/tools-zip/
 mkdir ~/.gf
 cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf/
 echo 'source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
