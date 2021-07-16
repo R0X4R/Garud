596961c77c82bf5269a273b1df3fe51c6a6c72d2
R0X4R <32596297+R0X4R@users.noreply.github.com>
2021-07-16 10:30:06 +0530

    Added `gf-patterns` collection
    Added some gf-patterns collection from different sources

--- install.sh
+++ install.sh
@@ -1,72 +1,68 @@
 #!/bin/bash
-
 echo "Make sure you're root before installing the tools"
 sleep 5s
 clear
-echo "Installing all dependencies"
-cd
-sudo apt-get install git
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo apt-get install ruby
-sudo apt-get install golang-go
-sudo apt install snapd
-sudo apt install cmake
-sudo apt install jq
-sudo snap install chromium
-sleep 2s
-clear
-echo "Installing python tools"
 mkdir -p ~/tools
 mkdir -p ~/tools/temp/
 mkdir -p ~/tools/payloads/
-wget https://raw.githubusercontent.com/R0X4R/Garud/master/payloads/ssti.txt
-wget https://raw.githubusercontent.com/R0X4R/Garud/master/payloads/lfi.txt
-mv lfi.txt ~/tools/payloads/
-mv ssti.txt ~/tools/payloads/
-git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r && cd ~/tools/Sublist3r && sudo pip3 install -r requirements.txt
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ~/tools/sqlmap/
-git clone https://github.com/ameenmaali/urldedupe.git ~/tools/urldedupe && cd ~/tools/urldedupe && cmake CMakeLists.txt && make && mv urldedupe /usr/bin/
-git clone https://github.com/codingo/Interlace.git ~/tools/interlace && cd ~/tools/interlace && python3 setup.py install
+mkdir -p ~/.gf
+mkdir -p ~/wordlists/
+mv payloads/patterns/*.json ~/.gf/
+mv payloads/lfi.txt ~/tools/payloads/
+mv payloads/ssti.txt ~/tools/payloads/
+echo "Installing all dependencies"
 cd
-mkdir ~/wordlists/
+sudo apt-get install git 2> /dev/null
+sudo apt-get install python3 2> /dev/null
+sudo apt-get install python3-pip 2> /dev/null
+sudo apt-get install ruby 2> /dev/null
+sudo apt-get install golang-go 2> /dev/null
+sudo apt install snapd 2> /dev/null
+sudo apt install cmake 2> /dev/null
+sudo apt install jq 2> /dev/null
+sudo snap install chromium 2> /dev/null
+sleep 2s
+clear
+echo "Installing python tools"
+git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r && cd ~/tools/Sublist3r && sudo pip3 install -r requirements.txt 2> /dev/null
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ~/tools/sqlmap/ 2> /dev/null
+git clone https://github.com/ameenmaali/urldedupe.git ~/tools/urldedupe && cd ~/tools/urldedupe && cmake CMakeLists.txt && make && mv urldedupe /usr/bin/ 2> /dev/null
+git clone https://github.com/codingo/Interlace.git ~/tools/interlace && cd ~/tools/interlace && python3 setup.py install 2> /dev/null
+echo "Installing Wordlists"
 cd ~/wordlists/ && wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/big.txt
 wget https://gist.githubusercontent.com/Lopseg/33106eb13372a72a31154e0bbab2d2b3/raw/a79331799a70d0ae0ea906f2b143996d85f71de5/dicc.txt
 https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/dns-Jhaddix.txt
 mv dns-Jhaddix.txt dns.txt
-clear
-echo "All python tools are installed"
 sleep 2s
 echo "Installing go-lang tools"
-go get github.com/michenriksen/aquatone
-go get -u github.com/tomnomnom/assetfinder
-GO111MODULE=on go get -u -v github.com/bp0lr/gauplus
-go get github.com/tomnomnom/waybackurls
-go get github.com/Emoe/kxss
-go get -u github.com/tomnomnom/qsreplace
-git clone https://github.com/projectdiscovery/dnsx.git; cd dnsx/cmd/dnsx; go build; mv dnsx /usr/bin/ &> /dev/null
-cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/notify.git; cd notify/cmd/notify; go build; mv notify /usr/bin/
-cd ~/tools/temp/ && git clone https://github.com/hahwul/dalfox && cd dalfox && go install
-cd ~/tools/temp/ && git clone https://github.com/dwisiswant0/crlfuzz && cd crlfuzz/cmd/crlfuzz && go build . && sudo mv crlfuzz /usr/bin/
-cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei && go build && mv nuclei /usr/bin/
-cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build && mv subfinder /usr/bin/
-cd ~/tools/temp/ && wget https://github.com/projectdiscovery/httpx/releases/download/v1.0.6/httpx_1.0.6_linux_386.tar.gz && tar -xvf httpx_1.0.6_linux_386.tar.gz && mv httpx /usr/bin/
-cd ~/tools/temp/ && wget https://github.com/OWASP/Amass/releases/download/v3.12.3/amass_linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/
+go get -u github.com/tomnomnom/anew 2> /dev/null
+go get -u github.com/tomnomnom/gf 2> /dev/null
+go get github.com/michenriksen/aquatone 2> /dev/null
+go get -u github.com/tomnomnom/assetfinder 2> /dev/null
+GO111MODULE=on go get -u -v github.com/bp0lr/gauplus 2> /dev/null
+go get github.com/tomnomnom/waybackurls 2> /dev/null
+go get github.com/Emoe/kxss 2> /dev/null
+go get -u github.com/tomnomnom/qsreplace 2> /dev/null
+git clone https://github.com/projectdiscovery/dnsx.git; cd dnsx/cmd/dnsx; go build; mv dnsx /usr/bin/ &> /dev/null 2> /dev/null
+cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/notify.git; cd notify/cmd/notify; go build; mv notify /usr/bin/ 2> /dev/null
+cd ~/tools/temp/ && git clone https://github.com/hahwul/dalfox && cd dalfox && go install 2> /dev/null
+cd ~/tools/temp/ && git clone https://github.com/dwisiswant0/crlfuzz && cd crlfuzz/cmd/crlfuzz && go build . && sudo mv crlfuzz /usr/bin/ 2> /dev/null
+cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei && go build && mv nuclei /usr/bin/ 2> /dev/null
+cd ~/tools/temp/ && git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go build && mv subfinder /usr/bin/ 2> /dev/null
+cd ~/tools/temp/ && wget https://github.com/projectdiscovery/httpx/releases/download/v1.0.6/httpx_1.0.6_linux_386.tar.gz && tar -xvf httpx_1.0.6_linux_386.tar.gz && mv httpx /usr/bin/ 2> /dev/null
+cd ~/tools/temp/ && wget https://github.com/OWASP/Amass/releases/download/v3.12.3/amass_linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/ 2> /dev/null
 cd ~/tools/temp/ && go get -u github.com/ffuf/ffuf
 cd ~/tools/temp/ && git clone https://github.com/R0X4R/Fuzzy.git
 cd Fuzzy
 chmod +x fuzzy && mv fuzzy /usr/bin/
 cd
 rm -rf ~/tools/temp/
-go get -u github.com/tomnomnom/anew
-go get -u github.com/tomnomnom/gf
-mkdir ~/.gf
 cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf/
 echo 'source ~/go/src/github.com/tomnomnom/gf/gf-completion.bash' >> ~/.bashrc
-git clone https://github.com/1ndianl33t/Gf-Patterns
+git clone https://github.com/1ndianl33t/Gf-Patterns 2> /dev/null
 mv ~/Gf-Patterns/*.json ~/.gf
 sudo cp ~/go/bin/* /usr/bin/
-nuclei -update-templates
+nuclei -update-templates &> /dev/null
 sleep 2s
 clear
 echo -e "Please add your slack webhook in ~/.config/notify/notify.conf file"
