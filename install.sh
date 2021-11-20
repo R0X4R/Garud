3ecbc17d0c2c6fa52760ca03dd9896f828dedec8
Eshan Singh <32596297+R0X4R@users.noreply.github.com>
2021-11-20 12:57:32 +0530

    fixed `install.sh` errors

--- install.sh
+++ install.sh
@@ -11,17 +11,19 @@ mv payloads/patterns/*.json ~/.gf/
 mv payloads/lfi.txt ~/tools/payloads/
 mv payloads/ssti.txt ~/tools/payloads/
 
-echo "Installing all dependencies\n"
+echo -e "Installing all dependencies\n"
 cd
 echo -e "- Installing git"
 sudo apt-get install git -y 2> /dev/null &> /dev/null
 sudo apt-get install unzip -y 2> /dev/null &> /dev/null
+
 echo -e "- Installing python"
 sudo apt-get install python3 -y 2> /dev/null &> /dev/null
 sudo apt-get install python3-pip -y 2> /dev/null &> /dev/null
 sudo apt install -y libpcap-dev 2> /dev/null &> /dev/null
 # sudo apt-get install ruby -y 2> /dev/null
 # sudo apt-get install golang-go -y 2> /dev/null
+
 echo -e "- Installing go-lang"
 wget https://golang.org/dl/go1.17.3.linux-amd64.tar.gz 2> /dev/null &> /dev/null
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.3.linux-amd64.tar.gz 2> /dev/null &> /dev/null
@@ -32,32 +34,53 @@ echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export GOPATH=$HOME/go'	>> ~/.bashrc			
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc	
 source ~/.bashrc
+
 echo -e "- Installing snapd"
 sudo apt install snapd -y 2> /dev/null &> /dev/null
+
 echo -e "- Installing cmake"
 sudo apt install cmake -y 2> /dev/null &> /dev/null
+
 echo -e "- Installing jq"
 sudo apt install jq -y 2> /dev/null &> /dev/null
 # sudo apt-get install -y parallel
+
 echo -e "- Installing chromium"
 sudo snap install chromium 2> /dev/null &> /dev/null
 sleep 2s
 
-echo "\nInstalling python tools\n"
+echo -e "\nInstalling python tools\n"
+
 echo -e "- Installing sublister"
-cd && git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r && cd ~/tools/Sublist3r && sudo pip3 install -r requirements.txt 2> /dev/null &> /dev/null
+cd
+git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r 2> /dev/null &> /dev/null
+cd ~/tools/Sublist3r
+sudo pip3 install -r requirements.txt 2> /dev/null &> /dev/null
+
 echo -e "- Installing sqlmap"
-cd && git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ~/tools/sqlmap/ 2> /dev/null &> /dev/null
+cd
+git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git ~/tools/sqlmap/ 2> /dev/null &> /dev/null
 # cd && git clone https://github.com/w9w/JSA.git ~/tools/JSA/ 2> /dev/null
+
 echo -e "- Installing urldedupe"
-cd && git clone https://github.com/ameenmaali/urldedupe.git ~/tools/.tmp/urldedupe && cd ~/tools/.tmp/urldedupe && cmake CMakeLists.txt && make && mv urldedupe /usr/bin/ 2> /dev/null &> /dev/null
+cd
+git clone https://github.com/ameenmaali/urldedupe.git ~/tools/.tmp/urldedupe 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/urldedupe
+cmake CMakeLists.txt 2> /dev/null &> /dev/null
+make 2> /dev/null &> /dev/null
+mv urldedupe /usr/bin/ 2> /dev/null &> /dev/null
+
 echo -e "- Installing interlace"
-cd && git clone https://github.com/codingo/Interlace.git ~/tools/interlace && cd ~/tools/interlace && python3 setup.py install 2> /dev/null &> /dev/null
+cd
+git clone https://github.com/codingo/Interlace.git ~/tools/interlace 2> /dev/null &> /dev/null
+cd ~/tools/interlace
+python3 setup.py install 2> /dev/null &> /dev/null
+
 echo -e "- Installing uro"
 cd && pip3 install tldextract 2> /dev/null &> /dev/null
 cd && pip3 install uro 2> /dev/null &> /dev/null
 
-echo "\nDownloading Wordlists\n"
+echo -e "\nDownloading Wordlists\n"
 cd ~/wordlists/
 # wget https://gist.githubusercontent.com/Lopseg/33106eb13372a72a31154e0bbab2d2b3/raw/a79331799a70d0ae0ea906f2b143996d85f71de5/dicc.txt
 echo -e "- Downloading dns wordlists"
@@ -70,57 +93,112 @@ echo -e "- Downloading fuzz wordlists"
 wget -q https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -O fuzz.txt 2> /dev/null &> /dev/null
 sleep 2s
 
-echo "\nInstalling go-lang tools\n"
+echo -e "\nInstalling go-lang tools\n"
+
 echo -e "- Installing anew"
 go install github.com/tomnomnom/anew@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing naabu"
 go install github.com/projectdiscovery/naabu/v2/cmd/naabu@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing gobuster"
 go install github.com/OJ/gobuster/v3@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing gf"
 go install github.com/tomnomnom/gf@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing gospider"
-cd && wget https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip -q 2> /dev/null &> /dev/null
-unzip gospider_v1.1.6_linux_x86_64.zip && cd gospider_v1.1.6_linux_x86_64 && mv gospider /usr/bin/ 2> /dev/null &> /dev/null
+cd
+wget https://github.com/jaeles-project/gospider/releases/download/v1.1.6/gospider_v1.1.6_linux_x86_64.zip -q 2> /dev/null &> /dev/null
+unzip gospider_v1.1.6_linux_x86_64.zip 2> /dev/null &> /dev/null
+cd gospider_v1.1.6_linux_x86_64 
+mv gospider /usr/bin/ 2> /dev/null &> /dev/null
 cd && rm -rf gospider*
+
 echo -e "- Installing aquatone"
 wget -q https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip 2> /dev/null &> /dev/null
 unzip aquatone_linux_amd64_1.7.0.zip 2> /dev/null &> /dev/null
 mv aquatone /usr/bin/
 rm -rf LICENSE.txt README.md
+
 echo -e "- Installing assetfinder"
 go install github.com/tomnomnom/assetfinder@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing crobat"
 go install github.com/cgboal/sonarsearch/cmd/crobat@latest 2> /dev/null &> /dev/null
 # GO111MODULE=on go get -u -v github.com/lc/subjs 2> /dev/null
+
 echo -e "- Installing gauplus"
 GO111MODULE=on go get -u -v github.com/bp0lr/gauplus 2> /dev/null &> /dev/null
+
 echo -e "- Installing waybackurls"
 go install github.com/tomnomnom/waybackurls@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing kxss"
 go install github.com/Emoe/kxss@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing qsreplace"
 go install github.com/tomnomnom/qsreplace@latest 2> /dev/null &> /dev/null
+
 echo -e "- Installing ffuf"
-cd ~/tools/.tmp/ && git clone https://github.com/ffuf/ffuf ; cd ffuf ; go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/ffuf/ffuf 2> /dev/null &> /dev/null
+cd ffuf
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing dnsx"
-cd ~/tools/.tmp/ && git clone https://github.com/projectdiscovery/dnsx.git; cd dnsx/cmd/dnsx; go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/projectdiscovery/dnsx.git 2> /dev/null &> /dev/null
+cd dnsx/cmd/dnsx
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing notify"
-cd ~/tools/.tmp/ && git clone https://github.com/projectdiscovery/notify.git; cd notify/cmd/notify; go build; mv notify /usr/bin/ 2> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/projectdiscovery/notify.git 2> /dev/null &> /dev/null
+cd notify/cmd/notify
+go build
+mv notify /usr/bin/ 2> /dev/null
+
 echo -e "- Installing dalfox"
-cd ~/tools/.tmp/ && git clone https://github.com/hahwul/dalfox && cd dalfox && go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/hahwul/dalfox 2> /dev/null &> /dev/null
+cd dalfox
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing crlfuzz"
-cd ~/tools/.tmp/ && git clone https://github.com/dwisiswant0/crlfuzz && cd crlfuzz/cmd/crlfuzz && go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/dwisiswant0/crlfuzz 2> /dev/null &> /dev/null
+cd crlfuzz/cmd/crlfuzz
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing nuclei"
-cd ~/tools/.tmp/ && git clone https://github.com/projectdiscovery/nuclei.git && cd nuclei/v2/cmd/nuclei && go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/projectdiscovery/nuclei.git 2> /dev/null &> /dev/null
+cd nuclei/v2/cmd/nuclei
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing subfinder"
-cd ~/tools/.tmp/ && git clone https://github.com/projectdiscovery/subfinder.git && cd subfinder/v2/cmd/subfinder && go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/projectdiscovery/subfinder.git
+cd subfinder/v2/cmd/subfinder
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing httpx"
-cd ~/tools/.tmp/ && git clone https://github.com/projectdiscovery/httpx.git && cd httpx/cmd/httpx && go install 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+git clone https://github.com/projectdiscovery/httpx.git
+cd httpx/cmd/httpx
+go install 2> /dev/null &> /dev/null
+
 echo -e "- Installing amass"
-cd ~/tools/.tmp/ && wget https://github.com/OWASP/Amass/releases/download/v3.12.3/amass_linux_i386.zip && unzip amass_linux_i386.zip && cd amass_linux_i386 && mv amass /usr/bin/ 2> /dev/null &> /dev/null
+cd ~/tools/.tmp/
+wget https://github.com/OWASP/Amass/releases/download/v3.12.3/amass_linux_i386.zip 2> /dev/null &> /dev/null
+unzip amass_linux_i386.zip 2> /dev/null &> /dev/null
+cd amass_linux_i386
+mv amass /usr/bin/ 2> /dev/null &> /dev/null
+
 cd
 sleep 3s
+
 echo -e "\nFinishing things up"
 rm -rf ~/tools/.tmp/ 2> /dev/null &> /dev/null
 cp -r ~/go/src/github.com/tomnomnom/gf/examples ~/.gf/ 2> /dev/null &> /dev/null
